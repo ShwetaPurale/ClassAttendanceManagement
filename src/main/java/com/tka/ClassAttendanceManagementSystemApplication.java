a7d6bbd18a0f97b1b20a720cf7e9a08dd1b5ed43
Shweta Purale <shwetapurale100gmail.com>
2024-11-21 10:59:51 +0530

    first commit

---
+++ src/main/java/com/tka/ClassAttendanceManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassAttendanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassAttendanceManagementSystemApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
